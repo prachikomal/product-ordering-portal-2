1a44ad98b145ae8aa6f51e4bc1af83f4e6e57ef7
prachi.komal <prachikkarn2015@gmail.com>
2023-06-30 12:27:04 +0530

    screen2_commit_products

--- src/components/Screen1.js
+++ src/components/Screen1.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, Paper, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Typography, Paper, List, ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import AssignmentIcon from '@material-ui/icons/Assignment';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
+
+const defaultimageurl="https://ik.imagekit.io/demo/medium_cafe_B1iTdD0C.jpg";
 const useStyles = makeStyles((theme) => ({
   container: {
     display: 'flex',
     height: '100vh',
     backgroundColor: 'grey'
+  },
+  backIcon:{
+   padding:"4px"
   },
   navbar: {
     flex: '0 0 20%',
@@ -28,6 +34,11 @@ const useStyles = makeStyles((theme) => ({
     overflowX: 'auto',
     padding: theme.spacing(2),
   },
+  itemDescriptionContainer :{
+    width: "200px" ,
+   textAlign:"center"
+  }
+  ,
   categoryItem: {
     display: 'flex',
     flexDirection: 'column',
@@ -35,12 +46,31 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(2),
     cursor: 'pointer',
   },
+  header:{
+   display:"flex",
+   flexDirection:'row',
+   
+  },
   paper: {
     borderRadius: '20px',
     margin: '10px',
     width: '80vw',
     padding: theme.spacing(2),
   },
+  productContent: {
+    display: 'flex',
+    flexWrap: 'wrap', 
+    justifyContent: 'space-around', 
+    alignItems: 'center', 
+    padding: theme.spacing(2),
+  },
+  itemDescription: {
+    wordWrap:'break-word',
+    /* Allow the text to wrap within the container */
+    width: '100%' /* Set the width to 100% to take the size of the paper container */
+  }
+  
+  
 }));
 
 const Screen1 = () => {
@@ -49,8 +79,11 @@ const Screen1 = () => {
   const [subCategories, setSubCategories] = useState([]);
   const [selectedSubCategory, setSelectedSubCategory] = useState(null);
 
+
   const [currentCategory, setCurrentCategories] = useState('Category 1');
   const [selectedOption, setSelectedOption] = useState('Allproducts');
+  const [products, setProducts] = useState([]);
+
 
   useEffect(() => {
     fetchCategories();
@@ -64,6 +97,7 @@ const Screen1 = () => {
       const data = await response.json();
       const result = data.result || [];
       setCategories(result);
+      console.log("categories",data);
     } catch (error) {
       console.error('Error fetching categories:', error);
     }
@@ -77,7 +111,8 @@ const Screen1 = () => {
       const data = await response.json();
       const result = data.result || [];
       setSubCategories(result);
-      setSelectedSubCategory(null);
+      console.log("subcategories",data);
+      
     } catch (error) {
       console.error('Error fetching Subcategories:', error);
     }
@@ -89,7 +124,10 @@ const Screen1 = () => {
         `https://elredtest.s3.amazonaws.com/reactAssignment/getProduct_${subCategoryId}.json`
       );
       const data = await response.json();
-console.log("products",data)
+console.log("products",data);
+const result = data.result || [];
+      setProducts(result);
+      
     } catch (error) {
       console.error('Error fetching products:', error);
     }
@@ -103,10 +141,11 @@ console.log("products",data)
     setCurrentCategories(options);
     fetchSubCategories(subcategory);
   };
-
+  
   const handleSubCategoryClick = (subCategoryId) => {
     setSelectedSubCategory(subCategoryId);
     fetchProducts(subCategoryId);
+    setSubCategories([]);
   };
 
   return (
@@ -155,7 +194,7 @@ console.log("products",data)
           </ListItem>
         </List>
       </div>
-
+      {!selectedSubCategory ? (
       <Paper className={classes.paper}>
         <Typography variant="h5">{currentCategory}</Typography>
         <div className={classes.content}>
@@ -167,7 +206,7 @@ console.log("products",data)
                 onClick={() => handleCategoryClick(category.categoryName, category.categoryId)}
               >
                 <img
-                  src={category.categoryImageURL || 'default-image-url'}
+                  src={category.categoryImageURL || defaultimageurl}
                   alt={category.categoryName}
                   height="100"
                   width="100"
@@ -191,7 +230,7 @@ console.log("products",data)
                 onClick={() => handleSubCategoryClick(subcategory.subCategoryId)}
               >
                 <img
-                  src={subcategory.subCategoryImageURL || 'default-image-url'}
+                  src={subcategory.subCategoryImageURL || defaultimageurl}
                   alt={subcategory.subCategoryName}
                   height="100"
                   width="100"
@@ -205,6 +244,44 @@ console.log("products",data)
         </div>
         
       </Paper>
+      ):(
+        <Paper className={classes.paper}>
+        <div className={classes.header}>
+
+        
+        <ArrowBackIcon className={classes.backIcon} />
+        <Typography variant="h6">All Products</Typography>
+        </div>
+    {/* content */}
+    <div className={classes.productContent}>
+          {products.length > 0 ? (
+            products.map((product) => (
+              <Paper
+                key={product.productId}
+                className={classes.categoryItem}
+               
+              >
+                <img
+  src={
+    (product.productImages && product.productImages.length > 0)
+      ? product.productImages[0]
+      : defaultimageurl
+  }
+  alt={product.itemDescription}
+  height="100"
+  width="100"
+/>
+<div className={classes.itemDescriptionContainer}>
+                <Typography variant="subtitle1"  className={classes.itemDescription}>{product.itemDescription}</Typography>
+             </div> 
+              </Paper>
+            ))
+          ) : (
+            <Typography variant="body1">No products found.</Typography>
+          )}
+        </div>
+  </Paper>
+      )}
     </div>
   );
 };
